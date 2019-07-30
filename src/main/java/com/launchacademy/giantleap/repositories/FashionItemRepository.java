9b8af1b0370194bd231481de987d47c505c711b1
Aygun90 <s.a.aygun.1990@gmail.com>
2019-07-30 16:12:56 -0400

    updating to master

--- src/main/java/com/launchacademy/giantleap/repositories/FashionItemRepository.java
+++ src/main/java/com/launchacademy/giantleap/repositories/FashionItemRepository.java
@@ -10,13 +10,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
 public interface FashionItemRepository extends PagingAndSortingRepository<FashionItem, Integer> {
-<<<<<<< HEAD
-  FashionItem findByName(String name);
-=======
   FashionItem findById(int id);
   FashionItem findByName(String name);
 
   @Query(value = "SELECT f FROM FashionItem f WHERE f.budget = :budget AND f.style = :style", nativeQuery = true)
   public Page<FashionItem> findByBudgetAndStyle(@Param("budget") Budget budget, @Param("style") Style style, Pageable pageable);
->>>>>>> c63072f23b281cb477d68c87b9140bca6bc3cef4
 }
