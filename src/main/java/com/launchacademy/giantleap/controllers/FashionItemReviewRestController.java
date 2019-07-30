c9134926788b93743a53d0084ffcfab3cf944360
amber <amber@UISMHLPT3598004.local>
2019-07-30 17:03:46 -0400

    Foundation is working, css files included as well as an added homePage.html is in progress.

--- src/main/java/com/launchacademy/giantleap/controllers/FashionItemReviewRestController.java
+++ src/main/java/com/launchacademy/giantleap/controllers/FashionItemReviewRestController.java
@@ -2,7 +2,7 @@ package com.launchacademy.giantleap.controllers;
 
 import com.launchacademy.giantleap.models.FashionItem;
 import com.launchacademy.giantleap.models.FashionItemReview;
-import com.launchacademy.giantleap.repositories.ItemReviewRepository;
+import com.launchacademy.giantleap.repositories.FashionItemReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -13,21 +13,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ItemReviewRestController {
-  private final ItemReviewRepository itemReviewRepository;
+public class FashionItemReviewRestController {
+  private final FashionItemReviewRepository fashionItemReviewRepository;
 
   @Autowired
-  public ItemReviewRestController(ItemReviewRepository itemReviewRepository){
-    this.itemReviewRepository = itemReviewRepository;
+  public FashionItemReviewRestController(FashionItemReviewRepository itemReviewRepository){
+    this.fashionItemReviewRepository = itemReviewRepository;
   }
 
   @GetMapping("api/v1/reviews/{id}")
   public Page<FashionItemReview> getFashionItems(@ModelAttribute FashionItem fashionItem, Pageable pageable){
-    return itemReviewRepository.findAllByFashionItem(fashionItem, pageable);
+    return fashionItemReviewRepository.findAllByFashionItem(fashionItem, pageable);
   }
 
   @PostMapping("api/v1/reviews/")
   public FashionItemReview addReview(@RequestBody FashionItemReview itemReview){
-    return itemReviewRepository.save(itemReview);
+    return fashionItemReviewRepository.save(itemReview);
   }
 }
